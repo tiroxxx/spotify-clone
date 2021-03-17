4d1b1f2e4ba2a29d5fd69f25d91301422f835792
Christian Marquez <christianmarquez41@gmail.com>
2021-03-17 17:49:20 -0400

    created dashboard component

--- server/server.js
+++ server/server.js
@@ -3,14 +3,22 @@ const SpotifyWebApi = require('spotify-web-api-node')
 
 const app = express()
 
-app.post("/login", (req,res) => {
+app.post("/login", (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
         redirectUri: "http//localhost:3000",
         clientId: "3a7598dc8dd04d0c9c1c7e45610c2573",
-        clientSecret: "c750f9b3327f4c9799d88797782a0606"
+        clientSecret:
     })
 
 
-    
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch(() => {
+        res.sendStatus(400)
+    })
 })
