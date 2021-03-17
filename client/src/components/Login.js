25d5318820a0d47154512bf10e6522d4289b741b
Christian Marquez <christianmarquez41@gmail.com>
2021-03-17 17:33:03 -0400

    added button to login with spotify

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Container } from "react-bootstrap"
 
 const redirectUri = "http://localhost:3000"
 const clientId = "3a7598dc8dd04d0c9c1c7e45610c2573"
@@ -7,8 +8,11 @@ const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&r
 
 export default function Login() {
     return (
-        <div>
-            
-        </div>
+        <Container className="d-flex justify-content-center 
+        align-items-center" style={{ minHeight: "100vh" }}>
+            <a className="btn btn-success btn-lg" href={AUTH_URL}>
+                Login with Spotify
+            </a>
+        </Container>
     )
 }
