a64b74ff820eaa36253a2dbb39aa484e69bc1700
Christian Marquez <christianmarquez41@gmail.com>
2021-03-17 20:23:32 -0400

    adding ability to refresh the spotify's access token

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -10,9 +10,18 @@ export default function useAuth(code) {
         axios.post("http://localhost:3001/login", {
             code
         }).then(res => {
-            console.log(res.data);
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            window.history.pushState({}, null, "/")
         }).catch(() => {
             window.location = "/"
         })
     }, [code])
+
+    useEffect(() => {
+
+    }, [refreshToken, expiresIn])
+
+    return accessToken
 }
--- server/server.js
+++ server/server.js
@@ -7,6 +7,27 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.post("refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken
+
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:3000",
+        clientId: "3a7598dc8dd04d0c9c1c7e45610c2573",
+        clientSecret: "c750f9b3327f4c9799d88797782a0606",
+        refreshToken
+    })
+
+    spotifyApi.refreshAccessToken().then(
+        data => {
+            console.log('The access token has been refreshed!');
+
+            // Save the access token so that it's used in future calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+        }).catch(() => {
+            res.sendStatus(400)
+        })
+})
+
 app.post("/login", (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
@@ -21,8 +42,7 @@ app.post("/login", (req, res) => {
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
         })
-    }).catch((err) => {
-        console.log(err);
+    }).catch(() => {
         res.sendStatus(400)
     })
 })
