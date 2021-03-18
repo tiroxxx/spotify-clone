e37e72f6eca35f7d60f5a81e0c6a154259340303
Christian Marquez <christianmarquez41@gmail.com>
2021-03-17 20:49:22 -0400

    added search bar and div for songs to the dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import Reac, { useState } from 'react'
+import { Container, Form } from 'react-bootstrap'
 import useAuth from "../hooks/useAuth"
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
+    const [search, setSearch] = useState()
     return (
-        <div>
-            {code}
-        </div>
+        <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+            <Form.Control
+                type="search"
+                placeholder="Search Songs/Artists"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+            />
+            <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+                Songs
+            </div>
+        </Container>
     )
 }
