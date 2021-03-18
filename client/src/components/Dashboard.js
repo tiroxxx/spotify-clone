8fc4e968d59c8cb2d501b01eef80fdbb57f19577
Christian Marquez <christianmarquez41@gmail.com>
2021-03-17 21:10:02 -0400

    console logged song results

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,47 @@
-import Reac, { useState } from 'react'
+import Reac, { useState, useEffect } from 'react'
 import { Container, Form } from 'react-bootstrap'
 import useAuth from "../hooks/useAuth"
+import SpotifyWebApi from "spotify-web-api-node"
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: "3a7598dc8dd04d0c9c1c7e45610c2573"
+})
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
-    const [search, setSearch] = useState()
+    const [search, setSearch] = useState("")
+    const [searchResults, setSearchResults] = useState([])
+    console.log(searchResults);
+
+    useEffect(() => {
+        if (!accessToken) return
+
+        spotifyApi.setAccessToken(accessToken)
+    }, [accessToken])
+
+    useEffect(() => {
+        if (!search) return setSearchResults([])
+        if (!accessToken) return
+
+        spotifyApi.searchTracks(search)
+            .then(res => {
+                setSearchResults(
+                    res.body.tracks.items.map(track => {
+                        const smallestAlbumImage = track.album.images.reduce((smallest, image) => {
+                            if (image.height < smallest.height) return image
+                            return smallest
+                        }, track.album.images[0])
+                        return {
+                            artist: track.artists[0].name,
+                            title: track.name,
+                            uri: track.uri,
+                            albumUrl: smallestAlbumImage.url
+                        }
+                    })
+                )
+            })
+    }, [search, accessToken])
+
     return (
         <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
             <Form.Control
