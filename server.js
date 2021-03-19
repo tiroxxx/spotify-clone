51eddb01d2efa12174afb59fa4b4251ed0a6fbfb
Christian Marquez <christianmarquez41@gmail.com>
2021-03-19 17:54:13 -0400

    fixed lyrics no showing

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
 }
