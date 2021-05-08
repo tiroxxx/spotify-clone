6b9af8d9c06405c29a946c9e45486bce7f1144a9
Christian Marquez <christianmarquez41@gmail.com>
2021-05-08 18:51:16 -0400

    adding styling to main and about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const redirectUri = "https://spotify-lyrical.herokuapp.com/"
 const clientId = "3a7598dc8dd04d0c9c1c7e45610c2573"
@@ -10,24 +10,39 @@ const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&r
 export default function About() {
     return (
         <main id="about-page">
-            <Navbar className="justify-content-center" expand="sm" variant="dark">
-                <Navbar.Brand href="/">
-                    <i style={{ marginRight: "2px" }} class="fab fa-spotify"></i>Spotify Lyrical
+            <Container id="nav-container" className="justify-content-center">
+                <Navbar className="justify-content-center" expand="sm" variant="dark">
+                    <Navbar.Brand href="/">
+                        <i style={{ marginRight: "2px" }} class="fab fa-spotify"></i>Spotify Lyrical
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse className="justify-content-center" id="responsive-navbar-nav">
-                    <Nav className="justify-content-center lol">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-                <a id="login-button" className="btn btn-success btn-md " href={AUTH_URL}>
-                    Login with Spotify
-                </a>
-            </Navbar>
-        <section id="about-section">
-            
-        </section>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-center" id="responsive-navbar-nav">
+                        <Nav className="justify-content-center lol">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/about">About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                    <a id="login-button" className="btn btn-success btn-md " href={AUTH_URL}>
+                        Login with Spotify
+                        </a>
+                </Navbar>
+            </Container>
+            <section id="about-section">
+                <Container id="about-text-area">
+                    <p className="about-text">Powered by Lyrics-Finder npm package</p>
+                    <p className="about-text">It scrapes google for the lyrics, which can sometimes result
+                        in no lyrics found if the song is not widely known</p>
+                </Container>
+            </section>
+            <footer id="footer">
+                <Container id="footer-container">
+                    <h3 style={{ fontSize: "1.25rem", marginBottom: "20px" }}>
+                        <i style={{ marginRight: "2px" }} class="fab fa-spotify"></i>Spotify Lyrical
+                        </h3>
+                    <p><a className="links" href="/">Home</a></p>
+                    <p><a className="links" href="/about">About</a></p>
+                </Container>
+            </footer>
         </main>
     )
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,38 +8,45 @@ const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&r
 
 export default function Login() {
     return (
-        <main id="login-page">
-            <Container id="login-nav-container" className="justify-content-center">
-                <Navbar className="justify-content-center" expand="sm" variant="dark">
-                    <Navbar.Brand href="/">
-                        <i style={{ marginRight: "2px" }} class="fab fa-spotify"></i>Spotify Lyrical
+        <>
+            <main id="login-page">
+                <Container id="nav-container" className="justify-content-center">
+                    <Navbar className="justify-content-center" expand="sm" variant="dark">
+                        <Navbar.Brand href="/">
+                            <i style={{ marginRight: "2px" }} class="fab fa-spotify"></i>Spotify Lyrical
                 </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse className="justify-content-center" id="responsive-navbar-nav">
-                        <Nav className="justify-content-center lol">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/about">About</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                    <a id="login-button" className="btn btn-success btn-md " href={AUTH_URL}>
-                        Login with Spotify
-                </a>
-                </Navbar>
-            </Container>
-            <div id="login-image">
-                <div id="login-text">
-                    <h1>
-                        Tired of singing you favorite song
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse className="justify-content-center" id="responsive-navbar-nav">
+                            <Nav className="justify-content-center lol">
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <Nav.Link href="/about">About</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                        <a id="login-button" className="btn btn-success btn-md " href={AUTH_URL}>
+                            Login with Spotify
+                        </a>
+                    </Navbar>
+                </Container>
+                <section id="login-image">
+                    <div id="login-text">
+                        <h1 className="login-title">
+                            Sing your favorite songs with
                     </h1>
-                    <h1>
-                        But not knowing the lyrics?!
+                        <h1 className="login-title">
+                            Spotify Lyrical
                     </h1>
-                    <h3>
-                        Welcome to Spotify Lyricial
-                    </h3>
-                </div>
-            </div>
-
-        </main>
+                    </div>
+                </section>
+                <footer id="footer">
+                    <Container id="footer-container">
+                        <h3 style={{ fontSize: "1.25rem", marginBottom: "20px"}}>
+                            <i style={{ marginRight: "2px" }} class="fab fa-spotify"></i>Spotify Lyrical
+                        </h3>
+                        <p><a className="links" href="/">Home</a></p>
+                        <p><a className="links" href="/about">About</a></p>
+                    </Container>
+                </footer>
+            </main>
+        </>
     )
 }
